e0be52285ca7e577764bd2c5fd0df235d42dd9a3
sadmk27 <83779736+sadmk27@users.noreply.github.com>
2026-05-15 14:07:06 +0200

    fix: betterauth infra install

--- apps/web/src/server/lib/auth.ts
+++ apps/web/src/server/lib/auth.ts
@@ -3,6 +3,7 @@ import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { db, schema } from "@portfolio/database";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { admin } from "better-auth/plugins/admin";
+import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
@@ -22,5 +23,5 @@ export const auth = betterAuth({
     enabled: true,
   },
 
-  plugins: [admin(), tanstackStartCookies()],
+  plugins: [admin(), tanstackStartCookies(), dash()],
 });
