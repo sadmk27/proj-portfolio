12ccb82dda3ea1125bc9aa1f3cee1ef97bf7be80
sadmk27 <83779736+sadmk27@users.noreply.github.com>
2026-05-15 11:09:58 +0200

    fix: update better auth base url and env file

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -3,12 +3,8 @@ import { adminClient } from "better-auth/client/plugins";
 
 const betterAuthURL =
   import.meta.env.VITE_BETTER_AUTH_URL ?? import.meta.env.BETTER_AUTH_URL;
-const baseURL =
-  typeof window === "undefined"
-    ? betterAuthURL
-    : (betterAuthURL ?? window.location.origin);
 
 export const authClient = createAuthClient({
-  baseURL,
+  baseURL: `${betterAuthURL}/api/auth`,
   plugins: [adminClient()],
 });
--- apps/web/src/server/lib/auth.ts
+++ apps/web/src/server/lib/auth.ts
@@ -5,6 +5,8 @@ import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { admin } from "better-auth/plugins/admin";
 
 export const auth = betterAuth({
+  baseUrl: process.env.BETTER_AUTH_URL,
+  basePath: "/api/auth",
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: {
